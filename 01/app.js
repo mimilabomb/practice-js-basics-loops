68fbe0518d1fd8356ff9ade5749217a2962e83ca
mimilabomb <bartoszsz13@gmail.com>
2024-06-04 17:34:14 +0200

    end first task

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,11 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log(randomNumber);
+
+
+if(randomNumber > 5){
+    for(let i = 5; i <= randomNumber; i++){
+        console.log(i);
+    }
+}else{
+    console.log('Wylosowana liczba jest zbyt mała, aby użyć pętli')
+}
