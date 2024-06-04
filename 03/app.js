a076af940ec4678b0125b293223973b5bc1eb00e
mimilabomb <bartoszsz13@gmail.com>
2024-06-04 18:03:46 +0200

    end third task

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,10 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while(randomNumber !== x) {
+    randomNumber = Math.round(Math.random() * 20);
+    iteration++;
+}
+
+console.log(iteration, 'Tyle iteracji było potrzebnych aby wylosować', x);
